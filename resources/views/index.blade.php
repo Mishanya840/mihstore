33a762ec1847eb18c5c0066deba7e7bc13ec7a2f
Mishanya <britovmisha840@gmail.com>
2015-10-30 19:26:58 +0300

    me first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Mihstore</title>
+	<link rel="stylesheet" type="text/css" href="http://mihstore.local/css/main.css">
+	<link rel="icon" type="imagex/x-icon" href="http://mihstore.local/images/favicon.ico">
+	<script type="text/javascript" src="http://mihstore.local/js/main.js"></script>
+</head>
+<body>
+	<header>
+		<div class="header_top">
+			<div class="header_top_text">The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.</div>
+			<ul>
+				<li><a href="#">my account</a></li>
+				<li><a href="#">wishust</a></li>
+				<li><a href="#">contact</a></li>
+			</ul>
+		</div>
+		<div class="header_center">
+			<img src="http://mihstore.local/images/icon.png" alt="Logo.image">
+			<ul>
+				<li class="vk"><a href="vk.com" target="_blank"></a></li>
+				<li class="fb"><a href="facebook.com"></a></li>
+				<li class="twitter"><a href="twitter.com"></a></li>
+				<li class="ya"><a href="yandex.ru"></a></li>
+				<li class="rss"><a href="rss.com"></a></li>
+			</ul>
+		</div>
+		<nav class="navigation_menu clearfix">
+			<ul class="navigation_menu_list">
+				<li class="products"><a href="#">products</a></li>
+				<li class="ideas"><a href="#">ideas</a></li>
+				<li class="brands"><a href="#">brands</a></li>
+				<li class="gifts"><a href="#">gifts</a></li>
+				<li class="stores"><a href="#">stores</a></li>
+			</ul>
+			<div class="shopping_cart"> 
+				<div class="shopping_cart_text">shopping cart:</div>
+				<div class="num_items_in_cart">3 items</div>
+				<div class="total_price">70$</div>
+				<div class="button_cart"></div>
+			</div>			
+		</nav>
+	<div class="description_line">
+		<div class="page_title">page title</div>
+		<div class="page_description">Page description The quick, brown fox jumps over a lazy dog. DJs flock by when TV ax quiz prog.</div> <!-- вылазиет если page_title сделать длинее -->
+	</div>
+	</header>
+	<aside class="page_right_navigation">
+		<div class="sort_products">
+			<div class="sort_products_title">sort products</div>
+			<div class="list">
+				<div class="list_item"><span class="text">Sort by <span class="red_word">prise</span></span></div>
+				<div class="list_item"><span class="text">Sort by <span class="red_word">style</span></span></div>
+				<div class="list_item"><span class="text">Sort by <span class="red_word">colour</span></span></div>
+				<div class="list_item"><span class="text">Sort by <span class="red_word">season</span></span></div>
+				<div class="list_item"><span class="text">Sort by <span class="red_word">rating</span></span></div>
+			</div>
+		</div>
+		<div class="best_sellers">
+			<div class="best_sellers_title">best sellers</div>
+			<div class="product_on_right_navigation">
+				<img src="images/example.jpg" alt="image">
+				<div class="product_on_right_navigation_container">
+					<div class="product_on_right_navigation_titile">Product name here</div>
+					<div class="product_on_right_navigation_description">Short description product</div>
+					<div class="product_on_right_navigation_price_title">Price:</div>
+					<div class="product_on_right_navigation_price">250$</div>
+				</div>
+			</div>
+			<div class="product_on_right_navigation">
+				<img src="images/example.jpg" alt="image">
+				<div class="product_on_right_navigation_container">
+					<div class="product_on_right_navigation_titile">Product name here</div>
+					<div class="product_on_right_navigation_description">Short description product</div>
+					<div class="product_on_right_navigation_price_title">Price:</div>
+					<div class="product_on_right_navigation_price">250$</div>
+				</div>
+			</div>
+			<div class="product_on_right_navigation">
+				<img src="images/example.jpg" alt="image">
+				<div class="product_on_right_navigation_container">
+					<div class="product_on_right_navigation_titile">Product name here</div>
+					<div class="product_on_right_navigation_description">Short description product</div>
+					<div class="product_on_right_navigation_price_title">Price:</div>
+					<div class="product_on_right_navigation_price">250$</div>
+				</div>
+			</div>
+			<div class="product_on_right_navigation">
+				<img src="images/example.jpg" alt="image">
+				<div class="product_on_right_navigation_container">
+					<div class="product_on_right_navigation_titile">Product name here</div>
+					<div class="product_on_right_navigation_description">Short description product</div>
+					<div class="product_on_right_navigation_price_title">Price:</div>
+					<div class="product_on_right_navigation_price">250$</div>
+				</div>
+			</div>
+		</div>
+		<div class="payment_options">
+			<div class="payment_options_title">payment options</div>
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+			<img class="payment_item" src="images/payment_1.png" alt="image_payment">
+		</div>
+	</aside>
+	<section class="page_content">
+		<img class="top_image_of_content" src="images/top_image_of_content.png" alt="top_image_of_content">
+		<div class="featured_products">
+			<div class="featured_products_title">featured products</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+			<div class="product">
+				<img src="images/product.jpg" alt="image_product">
+				<div class="product_button">
+					<div class="product_name">Cross</div>
+					<div class="product_price">50.00$</div>
+				</div>
+			</div>
+		</div>
+		<ul class="pagination">
+			<li id="num_1" onclick="pagination(this)"><a>1</a></li>
+			<li id="num_2" onclick="pagination(this)"><a>2</a></li>
+			<li id="num_3" onclick="pagination(this)"><a>3</a></li>
+			<li id="num_4" onclick="pagination(this)"><a>4</a></li>
+			<li id="num_5" onclick="pagination(this)"><a>5</a></li>
+			<li id="num_6" onclick="pagination(this)"><a>6</a></li>
+		</ul>
+	</section>
+	<footer>
+		<span>© All Rights Reserved - ICEwave Design</span>
+	</footer>
+	
+</body>
+</html>
